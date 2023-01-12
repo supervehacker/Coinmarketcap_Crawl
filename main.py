8595dd68ee4147c06681eef98ea7e4ed489e3378
Super Ve <huavinhvietanh@gmail.com>
2023-01-13 02:12:06 +0700

    master test2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ sys.stdout = Logger("mylog.log")  # redirect print function to log file
 # token_name = 'ruff'
 # token_name = 'ethereum'
 # xxxx main
-# xxxx main2
+# l_tokens = data_master.refresh_l_tokens()
 # l_tokens = data_master.refresh_l_tokens()
 l_tokens = refresh_l_tokens()
 
