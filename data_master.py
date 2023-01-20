7047c70152f21cc05bb9f1175a55a84694c48da2
Super Ve <huavinhvietanh@gmail.com>
2023-01-21 00:24:37 +0700

    try asyncio

--- data_master.py
+++ data_master.py
@@ -2,48 +2,87 @@ from data_processing.helpers import *
 import constants as c
 
 
+def refresh_l_tokens():
+    if c.RUN_DATE_HOUR == c.LAST_RUN_DATE_HOUR:
+        pass
+    else:  # c.RUN_DATE_HOUR != c.LAST_RUN_DATE_HOUR:
+        last_l_tokens_path, last_l_done_tokens_path, last_l_error_tokens_path = c.get_l_tokens_paths(
+            c.LAST_TOKEN_TO_CRAWL_PATH)
+        append_txt_to_txt(last_l_done_tokens_path, c.ALL_DONE_TOKENS_PATH)
+        append_txt_to_txt(last_l_error_tokens_path, c.ALL_ERROR_TOKENS_PATH)
+
+        all_tokens = read_list_from_txt(c.ALL_TOKENS_PATH)
+        all_done_tokens = read_list_from_txt_create_if_filenotfound(c.ALL_DONE_TOKENS_PATH)
+        all_error_tokens = read_list_from_txt_create_if_filenotfound(c.ALL_ERROR_TOKENS_PATH)
+        all_done_error_set = set(all_error_tokens + all_done_tokens)
+        all_undone_tokens = [x for x in all_tokens if x not in all_done_error_set]
+        write_list_to_txt(all_undone_tokens, c.ALL_UNDONE_TOKENS_PATH)
+
+    #
+    try:
+        l_tokens = read_list_from_txt(c.L_TOKENS_PATH)
+    except FileNotFoundError:
+        l_tokens = read_list_from_txt(c.ALL_UNDONE_TOKENS_PATH)[:100]  # TODO set this bigger?
+        # TODO get from all_tokens - (all_error_tokens + all_done_tokens)
+        write_list_to_txt(l_tokens, c.L_TOKENS_PATH)
+
+    l_done_tokens = read_list_from_txt_create_if_filenotfound(c.L_DONE_TOKENS_PATH)
+    l_error_tokens = read_list_from_txt_create_if_filenotfound(c.L_ERROR_TOKENS_PATH)
+
+    done_error_set = set(l_done_tokens + l_error_tokens)
+    l_tokens = [x for x in l_tokens if x not in done_error_set]
+
+    write_list_to_txt(l_tokens, c.L_TOKENS_PATH)
+
+    # Write RUN_DATE_HOUR to the file
+    with open(c.LAST_RUN_DATE_HOUR_PATH, "w") as f:
+        f.write(f"{c.RUN_DATE_HOUR}")
+
+    return l_tokens
+
+
 class DataMaster:
     def __init__(self):        # self.token_name = token_name        # self.all_tokens_to_crawl = all_tokens_to_crawl
         self.xxx = 1
 
-    @staticmethod
-    def refresh_l_tokens():
-
-        if c.RUN_DATE_HOUR == c.LAST_RUN_DATE_HOUR:
-            pass
-        else:  # c.RUN_DATE_HOUR != c.LAST_RUN_DATE_HOUR:
-            last_l_tokens_path, last_l_done_tokens_path, last_l_error_tokens_path = c.get_l_tokens_paths(c.LAST_TOKEN_TO_CRAWL_PATH)
-            append_txt_to_txt(last_l_done_tokens_path, c.ALL_DONE_TOKENS_PATH)
-            append_txt_to_txt(last_l_error_tokens_path, c.ALL_ERROR_TOKENS_PATH)
-
-            all_tokens = read_list_from_txt(c.ALL_TOKENS_PATH)
-            all_done_tokens = read_list_from_txt_create_if_filenotfound(c.ALL_DONE_TOKENS_PATH)
-            all_error_tokens = read_list_from_txt_create_if_filenotfound(c.ALL_ERROR_TOKENS_PATH)
-            all_done_error_set = set(all_error_tokens + all_done_tokens)
-            all_undone_tokens = [x for x in all_tokens if x not in all_done_error_set]
-            write_list_to_txt(all_undone_tokens, c.ALL_UNDONE_TOKENS_PATH)
-
-        #
-        try:
-            l_tokens = read_list_from_txt(c.L_TOKENS_PATH)
-        except FileNotFoundError:
-            l_tokens = read_list_from_txt(c.ALL_UNDONE_TOKENS_PATH)[:100]  # TODO set this bigger?
-            # TODO get from all_tokens - (all_error_tokens + all_done_tokens)
-            write_list_to_txt(l_tokens, c.L_TOKENS_PATH)
-
-        l_done_tokens = read_list_from_txt_create_if_filenotfound(c.L_DONE_TOKENS_PATH)
-        l_error_tokens = read_list_from_txt_create_if_filenotfound(c.L_ERROR_TOKENS_PATH)
-
-        done_error_set = set(l_done_tokens + l_error_tokens)
-        l_tokens = [x for x in l_tokens if x not in done_error_set]
-
-        write_list_to_txt(l_tokens, c.L_TOKENS_PATH)
-
-        # Write RUN_DATE_HOUR to the file
-        with open(c.LAST_RUN_DATE_HOUR_PATH, "w") as f:
-            f.write(f"{c.RUN_DATE_HOUR}")
-
-        return l_tokens
+    # @staticmethod
+    # def refresh_l_tokens():
+    #
+    #     if c.RUN_DATE_HOUR == c.LAST_RUN_DATE_HOUR:
+    #         pass
+    #     else:  # c.RUN_DATE_HOUR != c.LAST_RUN_DATE_HOUR:
+    #         last_l_tokens_path, last_l_done_tokens_path, last_l_error_tokens_path = c.get_l_tokens_paths(c.LAST_TOKEN_TO_CRAWL_PATH)
+    #         append_txt_to_txt(last_l_done_tokens_path, c.ALL_DONE_TOKENS_PATH)
+    #         append_txt_to_txt(last_l_error_tokens_path, c.ALL_ERROR_TOKENS_PATH)
+    #
+    #         all_tokens = read_list_from_txt(c.ALL_TOKENS_PATH)
+    #         all_done_tokens = read_list_from_txt_create_if_filenotfound(c.ALL_DONE_TOKENS_PATH)
+    #         all_error_tokens = read_list_from_txt_create_if_filenotfound(c.ALL_ERROR_TOKENS_PATH)
+    #         all_done_error_set = set(all_error_tokens + all_done_tokens)
+    #         all_undone_tokens = [x for x in all_tokens if x not in all_done_error_set]
+    #         write_list_to_txt(all_undone_tokens, c.ALL_UNDONE_TOKENS_PATH)
+    #
+    #     #
+    #     try:
+    #         l_tokens = read_list_from_txt(c.L_TOKENS_PATH)
+    #     except FileNotFoundError:
+    #         l_tokens = read_list_from_txt(c.ALL_UNDONE_TOKENS_PATH)[:100]  # TODO set this bigger?
+    #         # TODO get from all_tokens - (all_error_tokens + all_done_tokens)
+    #         write_list_to_txt(l_tokens, c.L_TOKENS_PATH)
+    #
+    #     l_done_tokens = read_list_from_txt_create_if_filenotfound(c.L_DONE_TOKENS_PATH)
+    #     l_error_tokens = read_list_from_txt_create_if_filenotfound(c.L_ERROR_TOKENS_PATH)
+    #
+    #     done_error_set = set(l_done_tokens + l_error_tokens)
+    #     l_tokens = [x for x in l_tokens if x not in done_error_set]
+    #
+    #     write_list_to_txt(l_tokens, c.L_TOKENS_PATH)
+    #
+    #     # Write RUN_DATE_HOUR to the file
+    #     with open(c.LAST_RUN_DATE_HOUR_PATH, "w") as f:
+    #         f.write(f"{c.RUN_DATE_HOUR}")
+    #
+    #     return l_tokens
 
     @staticmethod
     def manage_a_worker(token_name):
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("num_workers", type=int, help="number of concurrent workers")
 args = parser.parse_args()
+num_workers = args.num_workers
+
 # sys.stdout = Logger("mylog.log")  # redirect print function to log file
-
-
 # data_master = DataMaster()
 # l_tokens = data_master.refresh_l_tokens()
 # for token_name in l_tokens:
@@ -16,11 +16,11 @@ args = parser.parse_args()
 #     data_worker.crawl_single_token()
 #     break aa
 
-num_workers = args.num_workers
+
 # num_workers = 1
 
 print(f"Start the program with {num_workers} workers")
+l_tokens = refresh_l_tokens()
 
 data_master = DataMaster()
-l_tokens = data_master.refresh_l_tokens()
 data_master.manage_workers(num_workers=num_workers, l_tokens=l_tokens)
